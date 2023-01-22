373e3f76de7da5c4897f364e5b0d924832a8ca05
Vasyl Shtoiko <mr.shtoiko@gmail.com>
2023-01-22 14:18:08 +0200

    Create CollaboratorRequest.java

---
+++ src/main/java/com/softserve/itacademy/todolist/dto/CollaboratorRequest.java
@@ -0,0 +1,13 @@
+package com.softserve.itacademy.todolist.dto;
+
+public class CollaboratorRequest {
+    long collaborator_id;
+
+    public long getCollaboratorId() {
+        return collaborator_id;
+    }
+
+    public void setCollaboratorId(long collaboratorId) {
+        this.collaborator_id = collaboratorId;
+    }
+}
